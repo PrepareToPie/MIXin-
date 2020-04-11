78755af9a8728d5a461a3180bf40511912ab4a44
ilsur <subman450@gmail.com>
2020-04-12 00:39:45 +0300

    Fix

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -31,7 +31,7 @@ const Spotify = {
                         artist: track.artists[0].name,
                         album: track.album.name,
                         uri: track.uri
-                    }))
+                    }));
                 } else {
                     return [];
                 }
